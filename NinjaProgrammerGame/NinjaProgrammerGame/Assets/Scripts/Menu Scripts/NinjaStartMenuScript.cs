21cdbb919e95e6b32e4528044dab8a24b309ab2e
Xwz <kikopeyanski@gmail.com>
2015-06-22 16:10:02 +0300

    Added Start Menu Cool Thing
    Added

---
+++ NinjaProgrammerGame/NinjaProgrammerGame/Assets/Scripts/Menu Scripts/NinjaStartMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NinjaStartMenuScript : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+    public void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.CompareTag("ColliderGameStart"))
+        {
+            Debug.Log("trebva da po4ne");
+            Application.LoadLevel("Level 1");
+
+        }
+
+
+    }
+}
--- NinjaProgrammerGame/NinjaProgrammerGame/Assets/Scripts/PlayBtnClick.cs
+++ NinjaProgrammerGame/NinjaProgrammerGame/Assets/Scripts/PlayBtnClick.cs
@@ -3,10 +3,38 @@ using System.Collections;
 
 public class PlayBtnClick : MonoBehaviour 
 {
+    public GameObject player;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float forwardSpeed;
+
+
+    public void Start()
+    {
+        this.rb = player.GetComponent<Rigidbody2D>();
+        this.forwardSpeed = 0;
+        this.anim = player.GetComponent<Animator>();
+    }
 	public void PlayButtonClick()
 	{
 		//Application.LoadLevel("Level 1"); //ako ne se opravi level 1 she go mahneme i she imame samo 3 niva, sry :@:@
-		Application.LoadLevel("Level 2");
+        //Application.LoadLevel("Level 2");
 		Debug.Log("mai iskat da igraqt");
+
+        this.forwardSpeed = 5f;
+
+        this.anim.SetBool("didClick", true);
+
+
 	}
+
+    public void FixedUpdate()
+    {
+        var velocity = this.rb.velocity;
+        velocity.x = this.forwardSpeed;
+        this.rb.velocity = velocity;
+    }
+
+    
+
 }
